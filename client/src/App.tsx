de1d46a270d6d6dbabc66a9e43fa9d37db5a2e7b
Aliaksandr Samusevich <aliaksandr_samusevich@marpaihealth.com>
2021-03-15 15:57:44 +0300

    fix after rebase

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
 import './App.css';
 
-<<<<<<< HEAD
 const App: React.FC = () => (
   <div className="App">
     <h1>Hello World</h1>
   </div>
 );
-=======
-const App: React.FC = () => {
-  return (
-    <div className='App'>
-      <h1>Hello World</h1>
-    </div>
-  );
-};
->>>>>>> de78489... CM-8-1 Setup FE part
 
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,9 +6,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-<<<<<<< HEAD
   document.getElementById('root'),
-=======
-  document.getElementById('root')
->>>>>>> de78489... CM-8-1 Setup FE part
 );
