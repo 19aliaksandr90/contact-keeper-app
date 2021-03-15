c1201984aea9d201a4e09734c3fd0627d1aed047
Aliaksandr Samusevich <aliaksandr_samusevich@marpaihealth.com>
2021-03-15 16:01:22 +0300

    fix after rebase stage 2

--- client/src/setupTests.ts
+++ client/src/setupTests.ts
@@ -1,8 +1,1 @@
-<<<<<<< HEAD
-=======
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
->>>>>>> de78489... CM-8-1 Setup FE part
 import '@testing-library/jest-dom';
