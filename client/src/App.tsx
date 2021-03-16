54147fbd07cc9d96602df977b343530eb23cdc31
Aliaksandr Samusevich <aliaksandr_samusevich@marpaihealth.com>
2021-03-16 10:56:18 +0300

    basic implementation context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
 
 import Navbar from './components/layout/Navbar';
 import About from './components/pages/About';
 import Home from './components/pages/Home';
+import ContactState from './context/contacts/ContactState';
+
+import './App.css';
 
 const App: React.FC = () => (
-  <Router>
-    <>
-      <Navbar />
-      <div className="container">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-        </Switch>
-      </div>
-      My App
-    </>
-  </Router>
+  <ContactState>
+    <Router>
+      <Navbar>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+        My App
+      </Navbar>
+    </Router>
+  </ContactState>
 );
 
 export default App;
